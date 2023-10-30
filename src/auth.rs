4795ae82a1ed6c80d2380513a13c2c31097ec2e2
darryl west <darryl.west@raincitysoftware.com>
2023-10-30 10:02:05 -0700

    added todos

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,7 @@
 use lettre::transport::smtp::authentication::Credentials;
 use std::env;
 
+// TODO: change to RGMAILER with host, username, pw and return as a tuple
 const AUTH_KEY: &str = "EMAIL_CREDS";
 
 pub fn read_creds() -> Credentials {
@@ -17,6 +18,7 @@ pub fn read_creds() -> Credentials {
     let username = v[0].to_string();
     let password = v[1].to_string();
 
+    // TODO: refactor to just return generic username and password
     Credentials::new(username, password)
 }
 
