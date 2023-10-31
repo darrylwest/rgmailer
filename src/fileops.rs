103272dc03389a147045f880769742484cce4cdf
darryl west <darryl.west@raincitysoftware.com>
2023-10-31 13:28:25 -0700

    file system tests

--- src/fileops.rs
+++ src/fileops.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
 use std::fs;
-// use std::io;
 use std::ffi::OsStr;
 use std::path::{Component, PathBuf};
 
 // finds the absolute path; substibutes the filenames parent with to_target
-pub fn rename_from_to(filename: &str, to_target: &str) -> PathBuf {
+pub fn rename_from_to(filename: &str, to_target: &str) -> (PathBuf, PathBuf) {
     let path = PathBuf::from(filename);
     let abs_path = fs::canonicalize(path).unwrap();
 
@@ -18,15 +17,14 @@ pub fn rename_from_to(filename: &str, to_target: &str) -> PathBuf {
 
     let to_path = parts.iter().collect();
 
-    to_path
+    (abs_path, to_path)
 }
 
-pub fn move_to_sent(_filename: &str) -> Result<()> {
-    Ok(())
-}
-
-pub fn move_to_errors(_filename: &str) -> Result<()> {
-    Ok(())
+pub fn move_file(from: PathBuf, to: PathBuf) -> Result<()> {
+    match fs::rename(from, to) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.into())
+    }
 }
 
 #[cfg(test)]
@@ -36,13 +34,30 @@ mod tests {
     // create/call a function to build the test home strucure
 
     #[test]
+    fn test_move_to_sent() {
+        // remove the file first?
+        let filename = "tests/queue/sent-message.toml";
+        let (frompath, topath ) = rename_from_to(filename, "sent");
+        println!("from: {} to: {}", frompath.display(), topath.display());
+        let resp = fs::remove_file(topath.clone());
+        println!("{:?}", resp);
+
+        let resp = move_file(frompath, topath.clone()).unwrap();
+
+        println!("{:?}", resp);
+
+        let _ = fs::remove_file(topath.clone());
+    }
+
+    #[test]
     fn test_rename_from_to() {
         let filename = "tests/queue/7mNdj105Ch0c.toml";
 
-        let path = rename_from_to(filename, "sent");
+        let (frompath, topath ) = rename_from_to(filename, "sent");
 
-        println!("path: {}", path.display());
+        println!("from: {} to: {}", frompath.display(), topath.display());
 
-        assert!(path.ends_with("tests/sent/7mNdj105Ch0c.toml"));
+        assert!(frompath.ends_with("rgmailer/tests/queue/7mNdj105Ch0c.toml"));
+        assert!(topath.ends_with("rgmailer/tests/sent/7mNdj105Ch0c.toml"));
     }
 }
